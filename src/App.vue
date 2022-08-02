a0139130c1fc0bee450f7be76059f78d4f58048c
tch1001 <tanchienhao@gmail.com>
2022-08-02 20:25:20 +0800

    working graph resize

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,26 @@
 <template>
+  <h1>TwigSlot</h1>
   <div id="graph">
-    <v-network-graph :nodes="nodes" :edges="edges"/>
+    <v-network-graph 
+    v-model:zoom-level="zoomLevel"
+    :nodes="nodes"
+    :edges="edges"
+    :configs="configs"
+    />
   </div>
   <text id="graph-div-error">ERROR: This message will disappear when the graph div is resized appropriately.</text>
 </template>
 
 <script>
 import { defineComponent } from "vue"
+import { reactive, ref } from "vue"
+import * as vNG from "v-network-graph"
+import {
+  ForceLayout,
+  ForceNodeDatum,
+  ForceEdgeDatum,
+} from "v-network-graph/lib/force-layout"
+// import $ from "jquery";
 export default defineComponent({
   setup() {
     const nodes = {
@@ -20,10 +34,26 @@ export default defineComponent({
       edge2: { source: "node2", target: "node3" },
       edge3: { source: "node3", target: "node4" },
     }
-    return { nodes, edges }
+    const configs = reactive(
+      vNG.defineConfigs({
+        view: {
+          scalingObjects: true,
+          minZoomLevel: 0.1,
+          maxZoomLevel: 16,
+        },
+      })
+    )
+    const zoomLevel = ref(1.5)
+    return { nodes, edges, configs, zoomLevel }
   },
-  watch: {
-
+  mounted(){
+    document.getElementById('graph-div-error').remove()
+    var graph = document.getElementById('graph');
+    const windowHeight = window.innerHeight;
+    const windowWidth = window.innerWidth;
+    const titleHeight = 100;
+    graph.style.setProperty('height',`${windowHeight-titleHeight}px`)
+    graph.style.setProperty('width',`${windowWidth*0.6}px`)
   }
 })
 </script>
@@ -37,7 +67,7 @@ export default defineComponent({
   color: #2c3e50;
   background-color: aqua;
   /* margin-top: 60px; */
-  height: 20px;
+  height: 10000px;
   overflow: hidden;
 }
 </style>
