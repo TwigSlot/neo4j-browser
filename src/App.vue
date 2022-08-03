25c56ea1c72fec632f4863a98fa7b15024f279b1
tch1001 <tanchienhao@gmail.com>
2022-08-03 10:39:02 +0800

    working add node

--- src/App.vue
+++ src/App.vue
@@ -4,9 +4,11 @@
       <label for="d3-force-enabled">Auto-organise</label>
   <div id="graph">
     <v-network-graph 
+    ref="graph"
     v-model:zoom-level="zoomLevel"
     :nodes="nodes"
     :edges="edges"
+    :layouts="layouts"
     :configs="configs"
     :event-handlers="eventHandlers"
     />
@@ -15,39 +17,36 @@
 </template>
 <script setup>
 console.log(eventLogs)
-</script>
-
-<script>
-import { defineComponent, reactive, ref, computed } from "vue"
-import * as vNG from "v-network-graph"
-import {
-  ForceLayout,
-} from "v-network-graph/lib/force-layout"
-
-const original_nodes = {
-  node1: { name: "Node 1" },
-  node2: { name: "Node 2" },
-  node3: { name: "Node 3" },
-  node4: { name: "Node 4" },
+function setHandler(mode){
+  if(mode == 'vertex'){
+    return;
+  }
 }
-var original_edges = {
-  edge1: { source: "node1", target: "node2" },
-  edge2: { source: "node2", target: "node3" },
-  edge3: { source: "node3", target: "node4" },
+document.onkeydown = function(e){
+  if(e.key == 'v') setHandler('vertex')
 }
-const nodes = reactive({ ...original_nodes })
-const edges = reactive({ ...original_edges })
-const nextNodeIndex = ref(Object.keys(nodes).length + 1)
-const nextEdgeIndex = ref(Object.keys(edges).length + 1)
+document.viewClick = function(e){
+  console.log('view click', e)
+  // var boundingRect = document.getElementsByClassName('v-canvas')[0].getBoundingClientRect();
+  // var viewBox = window.vue.graph.getViewBox();
+  // const percentageX = (e.x - boundingRect.x)/(boundingRect.right - boundingRect.x)
+  // const graphX = (1-percentageX) * viewBox.left + (percentageX) * viewBox.right;
 
-const selectedNodes = ref();
-const selectedEdges = ref();
-const eventLogs = reactive([])
+  // const percentageY = (e.y - boundingRect.y)/(boundingRect.bottom- boundingRect.y)
+  // const graphY = (1-percentageY) * viewBox.top + (percentageY) * viewBox.bottom;
+  const point = { x: e.offsetX, y: e.offsetY }
+  // translate coordinates: DOM -> SVG
+  const svgPoint = window.vue.graph.translateFromDomToSvgCoordinates(point)
 
-function addNode() {
+  // add node and its position
+  addNode(svgPoint.x, svgPoint.y);
+}
+
+function addNode(x, y) {
   const nodeId = `node${nextNodeIndex.value}`
-  const name = `N${nextNodeIndex.value}`
+  const name = `Node ${nextNodeIndex.value}`
   nodes[nodeId] = { name }
+  window.vue.layouts.nodes[nodeId] = {x: x, y: y};
   nextNodeIndex.value++
 }
 
@@ -70,6 +69,36 @@ function removeEdge() {
     delete edges[edgeId]
   }
 }
+</script>
+
+<script>
+import { defineComponent, reactive, ref, computed } from "vue"
+import * as vNG from "v-network-graph"
+import {
+  ForceLayout,
+} from "v-network-graph/lib/force-layout"
+
+const original_nodes = {
+  node1: { name: "Node 1" },
+  node2: { name: "Node 2" },
+  node3: { name: "Node 3" },
+  node4: { name: "Node 4" },
+}
+var original_edges = {
+  edge1: { source: "node1", target: "node2" },
+  edge2: { source: "node2", target: "node3" },
+  edge3: { source: "node3", target: "node4" },
+}
+const graph = ref();
+const nodes = reactive({ ...original_nodes })
+const edges = reactive({ ...original_edges })
+const nextNodeIndex = ref(Object.keys(nodes).length + 1)
+const nextEdgeIndex = ref(Object.keys(edges).length + 1)
+
+const selectedNodes = ref();
+const selectedEdges = ref();
+const eventLogs = reactive([])
+
 export default defineComponent({
   data() {
     
@@ -111,17 +140,30 @@ export default defineComponent({
     const eventHandlers = {
       // wildcard: capture all events
       "*": (type, event) => {
-        if (eventLogs.length > EVENTS_COUNT) {
-          eventLogs.splice(EVENTS_COUNT, eventLogs.length - EVENTS_COUNT)
+        // if (eventLogs.length > EVENTS_COUNT) {
+        //   eventLogs.splice(EVENTS_COUNT, eventLogs.length - EVENTS_COUNT)
+        // }
+        // if (event instanceof Object && "event" in event) {
+        //   Object.assign(event, { event: "(...)" })
+        // }
+        // eventLogs.unshift([type, JSON.stringify(event)])
+        if(type == 'view:click'){
+          document.viewClick(event.event);
+        }else{
+          console.log(type, event)
         }
-        if (event instanceof Object && "event" in event) {
-          Object.assign(event, { event: "(...)" })
-        }
-        eventLogs.unshift([type, JSON.stringify(event)])
       },
     }
     const zoomLevel = ref(1.5)
-    return { nodes, edges, configs, zoomLevel, d3ForceEnabled, eventHandlers }
+    const layouts = reactive({
+      nodes: {
+        node1: { x: 0, y: 0 },
+        node2: { x: 50, y: 50 },
+        node3: { x: 100, y: 0 },
+        node4: { x: 150, y: 50 },
+      },
+    });
+    return { graph, nodes, edges, configs, layouts, zoomLevel, d3ForceEnabled, eventHandlers }
   },
   mounted(){
     document.getElementById('graph-div-error').remove()
