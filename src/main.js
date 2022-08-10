0caed08fa34893605b2828db86c89fadfa0f944a
Robert <xiurobert@gmail.com>
2022-08-10 16:42:04 +0800

    Cleanup UI

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import { createApp } from "vue"
 import VNetworkGraph from "v-network-graph"
 import "v-network-graph/lib/style.css"
 import App from "./App.vue"
+import 'bulma/css/bulma.css'
 
 const app = createApp(App)
 
